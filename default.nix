fe71b118770bba65d0cc3aa341626a936339cd69
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-09-12 18:25:21 +0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -18,18 +18,20 @@ pkgs.stdenv.mkDerivation {
   ];
 
   buildPhase = ''
-    mkdir -p $out/bin
-    cp -r * $out/bin
-  '';
+  mkdir -p $out/bin
+'';
 
   installPhase = ''
-    cp -f ./anti-hangman.sh $out/bin/anti-hangman
-    chmod +x $out/bin/anti-hangman
-  '';
+  mkdir -p $out/share/doc/anti-hangman
+  cp -f ./LICENSE $out/share/doc/anti-hangman/LICENSE
+  cp -f ./README.md $out/share/doc/anti-hangman/README.md
+  cp -f ./anti-hangman.sh $out/bin/anti-hangman
+  chmod +x $out/bin/anti-hangman
+'';
 
   meta = with pkgs.lib; {
     description = "Anti-hangman like hangman games but it's difference, Using permutations method written in Bash script.";
-    license = licenses.gplv3.0;
+    license = licenses.gpl3;
     maintainers = with maintainers; [ luisadha ];
   };
 }
